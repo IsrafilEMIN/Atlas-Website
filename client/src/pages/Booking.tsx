163f5338d6f93472b94e4fd1ffcf957c18d16282
IsrafilEMIN <98862614+IsrafilEMIN@users.noreply.github.com>
2025-02-11 12:43:57 +0000

    SendGrid implementation

--- client/src/pages/Booking.tsx
+++ client/src/pages/Booking.tsx
@@ -47,7 +47,6 @@ export default function Booking() {
         throw new Error('Failed to book appointment');
       }
 
-      //
       // Reset form
       form.reset();
       setDate(undefined);
