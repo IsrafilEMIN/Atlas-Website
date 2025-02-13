786202197062dcfbf4a4534785e8d90d4f1b940d
Kutluk <iso.general@icloud.com>
2025-02-12 23:09:39 -0500

    added phone call action to footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -80,7 +80,9 @@ export default function Footer() {
             <ul className="space-y-2">
               <li className="flex items-center text-gray-300">
                 <Phone className="w-5 h-5 mr-2" />
-                <span>(647) 916-0826</span>
+                <a href="tel:+16479160826" className="hover:underline">
+                  (647) 916-0826
+                </a>
               </li>
               <li className="flex items-center text-gray-300">
                 <Mail className="w-5 h-5 mr-2" />
