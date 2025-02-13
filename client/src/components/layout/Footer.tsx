f4655fb7f509f3b5610502dbcab980491c55428b
Kutluk <iso.general@icloud.com>
2025-02-12 21:11:47 -0500

    fixed the url for Home

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
             <h3 className="text-lg font-semibold text-gray-300 mb-4">Quick Links</h3>
             <ul className="space-y-2">
               <li>
-                <Link href="/home">
+                <Link href="/">
                   <a className="text-gray-300 hover:underline">Home</a>
                 </Link>
               </li>
